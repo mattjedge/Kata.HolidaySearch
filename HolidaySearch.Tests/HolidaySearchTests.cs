2b4e950c2aa3950eeaf95af2bbe71a0eac8492d9
mattjedge <matt.jj.edge@gmail.com>
2025-02-26 00:06:27 +0000

    HolidaySearch to bring it all together (it got late but wanted to wrap it up)

--- HolidaySearch.Tests/HolidaySearchTests.cs
+++ HolidaySearch.Tests/HolidaySearchTests.cs
@@ -1,12 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HolidaySearch.Models;
+using HolidaySearch.Search;
+using System.Text.Json;
 
 namespace HolidaySearch.Tests
 {
     internal class HolidaySearchTests
     {
+        private IEnumerable<FlightData> _flightData;
+        private IEnumerable<HotelData> _hotelData;
+        private HolidaySearch _subject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _flightData = SeedFlightData();
+            var flightSearch = new FlightSearch(_flightData);
+            _hotelData = SeedHotelData();
+            var hotelSearch = new HotelSearch(_hotelData);
+
+            _subject = new HolidaySearch(hotelSearch, flightSearch);
+        }
+
+        [Test]
+        public void CustomerOne()
+        {
+            var query = new HolidaySearchQuery(["MAN"], ["AGP"], new DateOnly(2023, 07, 01), 7);
+            var results = _subject.SearchHolidays(query);
+            Assert.That(results.First().Flight.Id, Is.EqualTo(2));
+            Assert.That(results.First().Hotel.Id, Is.EqualTo(9));
+        }
+
+        [Test]
+        public void CustomerTwo()
+        {
+            var query = new HolidaySearchQuery(["LGW", "LTN"], ["PMI"], new DateOnly(2023, 06, 15), 10);
+            var results = _subject.SearchHolidays(query);
+            Assert.That(results.First().Flight.Id, Is.EqualTo(6));
+            Assert.That(results.First().Hotel.Id, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void CustomerThree()
+        {
+            var query = new HolidaySearchQuery([], ["LPA"], new DateOnly(2022, 11, 10), 14);
+            var results = _subject.SearchHolidays(query);
+            Assert.That(results.First().Flight.Id, Is.EqualTo(7));
+            Assert.That(results.First().Hotel.Id, Is.EqualTo(6));
+        }
+        
+        private static IEnumerable<FlightData> SeedFlightData()
+        {
+            var flightDataString = File.ReadAllText(@"Data\flight-data.json");
+            return JsonSerializer.Deserialize<IEnumerable<FlightData>>(flightDataString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+
+        private static IEnumerable<HotelData> SeedHotelData()
+        {
+            var hotelDataString = File.ReadAllText(@"Data\hotel-data.json");
+            return JsonSerializer.Deserialize<IEnumerable<HotelData>>(hotelDataString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
     }
 }
---
+++ HolidaySearch/HolidaySearch.cs
@@ -0,0 +1,72 @@
+﻿using HolidaySearch.FilterStrategies;
+using HolidaySearch.Models;
+using HolidaySearch.Search;
+
+namespace HolidaySearch
+{
+    public record HolidaySearchQuery(string[] DepartingFrom, string[] TravelingTo, DateOnly DepartureDate, int Duration);
+    public record HolidaySearchResult(FlightData Flight, HotelData Hotel);
+
+    public class HolidaySearch
+    {
+        private ISearch<HotelData> _hotelSearch;
+        private ISearch<FlightData> _flightSearch;
+
+        public HolidaySearch(ISearch<HotelData> hotelSearch, ISearch<FlightData> flightSearch)
+        {
+            _hotelSearch = hotelSearch;
+            _flightSearch = flightSearch;
+        }
+
+        public IEnumerable<HolidaySearchResult> SearchHolidays(HolidaySearchQuery query)
+        {
+            var hotelFilters = GenerateHotelSearchFilters(query);
+            var flightFilters = GenerateFlightSearchFilters(query);
+
+            var hotelResults = _hotelSearch.Search(hotelFilters);
+            var flightResults = _flightSearch.Search(flightFilters);
+
+            return CreatePackageHolidaySearchResults(hotelResults, flightResults);
+        }
+
+        public IEnumerable<HolidaySearchResult> CreatePackageHolidaySearchResults(IEnumerable<HotelData> hotels, IEnumerable<FlightData> flights)
+        {
+            return hotels.SelectMany(hotel => flights,
+                (hotel, flight) => new HolidaySearchResult(flight, hotel))
+                .OrderBy(package => package.Flight.Price + package.Hotel.PricePerNight)
+                .ToList();
+        }
+        
+        private IEnumerable<IFilterStrategy<HotelData>> GenerateHotelSearchFilters(HolidaySearchQuery query)
+        {
+            var filters = new List<IFilterStrategy<HotelData>>
+            {
+                new DurationFilterStrategy(query.Duration),
+                new DatesFilterStrategy<HotelData>(query.DepartureDate, x => x.ArrivalDate)
+            };
+
+            if (query.TravelingTo.Any())
+            {
+                filters.Add(new DestinationFilterStrategy<HotelData>(query.TravelingTo, x => x.LocalAirports));
+            }
+
+            return filters;
+        }
+
+        private IEnumerable<IFilterStrategy<FlightData>> GenerateFlightSearchFilters(HolidaySearchQuery query)
+        {
+            var filters = new List<IFilterStrategy<FlightData>>
+            {
+                new DestinationFilterStrategy<FlightData>(query.TravelingTo, x => [x.To]),
+                new DatesFilterStrategy<FlightData>(query.DepartureDate, x => x.DepartureDate)
+            };
+
+            if (query.DepartingFrom.Any())
+            {
+                filters.Add(new DepartureLocationFilterStrategy(query.DepartingFrom));
+            }
+
+            return filters;
+        }
+    }
+}
